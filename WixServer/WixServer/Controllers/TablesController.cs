41ff8257fa0940b854e5cc72015de31fd2b02539
yuval <yuvalgit@gmail.com>
2016-04-28 16:02:19 +0300

    Added the table controller

---
+++ WixServer/WixServer/Controllers/TablesController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WixServer.Models;
+
+namespace WixServer.Controllers
+{
+    public class TablesController : ApiController
+    {
+        private WixServerContext db = new WixServerContext();
+
+        // GET: api/Tables/5
+        [ResponseType(typeof(Table))]
+        public IHttpActionResult GetTable(int id)
+        {
+            Table table = db.Tables.Find(id);
+            if (table == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(table);
+        }
+
+        // PUT: api/Tables/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutTable(int id, Table table)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != table.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(table).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TableExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Tables
+        [ResponseType(typeof(Table))]
+        public IHttpActionResult PostTable(Table table)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Tables.Add(table);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = table.Id }, table);
+        }
+
+        // DELETE: api/Tables/5
+        [ResponseType(typeof(Table))]
+        public IHttpActionResult DeleteTable(int id)
+        {
+            Table table = db.Tables.Find(id);
+            if (table == null)
+            {
+                return NotFound();
+            }
+
+            db.Tables.Remove(table);
+            db.SaveChanges();
+
+            return Ok(table);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool TableExists(int id)
+        {
+            return db.Tables.Count(e => e.Id == id) > 0;
+        }
+
+        // GET: api/Tables
+        public IQueryable<Table> GetTables()
+        {
+            return db.Tables;
+        }
+    }
+}
